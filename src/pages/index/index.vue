201c908dc846dc7f986907f78aa746038a72ebb4
hongzhan <hongzhan@cestc.cn>
2025-11-26 15:29:54 +0800

    fix: 100px

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -97,7 +97,7 @@
           </view>
         </view>
         <view class="user-info-cards" style="display: flex; flex-direction: column; align-items: center;">
-          <uv-image src="/static/vxqrcode.png" :lazy-load="true" height="205" width="205"></uv-image>
+          <uv-image src="/static/vxqrcode.png" :lazy-load="true" height="100" width="100"></uv-image>
           <view class="card-title">作者微信：xzml8char 工作日晚5点以后统一回复，子时不问事 </view>
         </view>
       </uv-form>
